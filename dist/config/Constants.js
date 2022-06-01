971289590320b26396e696b1dd23d7e2a49bb906
lucascasagrande98 <lucascasagrande98@gmail.com>
2022-06-01 02:03:15 -0300

    first commit

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: 'mongodb+srv://admin:6uaOoEv9wgPHvgHn@url-shortnner-dio.mbyuso7.mongodb.net/?retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+  API_URL: "http://localhost:5000",
+  MONGO_CONNECTION:
+  'mongodb+srv://admin:6uaOoEv9wgPHvgHn@url-shortnner-dio.mbyuso7.mongodb.net/?retryWrites=true&w=majority',
+};
